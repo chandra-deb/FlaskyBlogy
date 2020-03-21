44e4ecda3b1849f09a3db3ef7d0c021440b88545
chandra-deb <mail.chandradeb@gmail.com>
2020-03-21 13:17:29 +0600

    login manager and generate hash password and check

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import  Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 app.config.from_object(Config)
 
 from app import  routes, models
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 from datetime import datetime
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +17,13 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     def __repr__(self):
         return 'Post <{}>'.format(self.body)
